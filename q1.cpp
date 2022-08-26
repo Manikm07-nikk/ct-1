cb06a7682e7fb179d09a6dcf96f2249d766ac889
Manikm07-nikk <110350100+Manikm07-nikk@users.noreply.github.com>
2022-08-26 14:00:16 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,54 @@
+//Name= MANIK
+//ROLL No.= 2010990442
+//Set No.= 3
+//Q1.
+
+
+#include <iostream>
+
+using namespace std;
+ 
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+ 
+
+void rearrangeArray(int arr[], int n)
+{
+    
+    for (int i = 1; i < n; i += 2)
+    {
+        
+        if (arr[i - 1] > arr[i]) {
+            swap(arr, i - 1, i);
+        }
+ 
+       
+        if (i + 1 < n && arr[i + 1] > arr[i]) {
+            swap(arr, i + 1, i);
+        }
+    }
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+   for(int i=0;i<n;i++)
+   {
+       cin>>arr[i];
+   }
+ 
+    rearrangeArray(arr, n);
+ 
+   
+    for (int i = 0; i < n; i++) {
+        cout<< arr[i]<<" ";
+    }
+ 
+    return 0;
+}
